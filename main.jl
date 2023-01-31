ed0be8b54f0971c341d8236b335889f9892f1eb7
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2023-01-31 15:04:08 -0800

    Update main.jl

--- main.jl
+++ main.jl
@@ -1,40 +1,43 @@
 using QuantumOptics, CairoMakie
 
 
-Npoints = 640
-Npointsy = 480
-scale = .1
+Npoints = 160
+Npointsy = 120
 
-b_position = PositionBasis(-Npoints*scale/2, Npoints*scale/2, Npoints)
+xmin = -30
+xmax = 50
+b_position = PositionBasis(xmin, xmax, Npoints)
 b_momentum = MomentumBasis(b_position)
 
-b_positiony = PositionBasis(-Npointsy*scale/2, Npointsy*scale/2, Npointsy)
-b_momentumy = MomentumBasis(b_positiony);
+ymin = -20
+ymax = 20
+b_positiony = PositionBasis(ymin, ymax, Npointsy)
+b_momentumy = MomentumBasis(b_positiony)
 
 
 b_comp_x = b_position ⊗ b_positiony
 b_comp_p = b_momentum ⊗ b_momentumy
 
 Txp = transform(b_comp_x, b_comp_p)
-Tpx = transform(b_comp_p, b_comp_x);
+Tpx = transform(b_comp_p, b_comp_x)
 
 
 px = momentum(b_momentum)
-py = momentum(b_momentumy);
+py = momentum(b_momentumy)
 
 
 Hkinx = LazyTensor(b_comp_p, [1, 2], (px^2/2, one(b_momentumy)))
 Hkiny = LazyTensor(b_comp_p, [1, 2], (one(b_momentum), py^2/2))
 
 Hkinx_FFT = LazyProduct(Txp, Hkinx, Tpx)
-Hkiny_FFT = LazyProduct(Txp, Hkiny, Tpx);
+Hkiny_FFT = LazyProduct(Txp, Hkiny, Tpx)
 
 
 potential(x,y) = exp(-(x^2 + y^2)/30.0)
-V = potentialoperator(b_comp_x, potential);
+V = potentialoperator(b_comp_x, potential)
 
 
-H = LazySum(Hkinx_FFT, Hkiny_FFT, V);
+H = LazySum(Hkinx_FFT, Hkiny_FFT, V)
 
 
 x0 = -10
@@ -48,7 +51,13 @@ p0_y = 0.5
 ψ = ψx ⊗ ψy
 
 T = collect(0.0:0.1:20.0)
-tout, ψt = timeevolution.schroedinger(T, ψ, H);
+tout, ψt = timeevolution.schroedinger(T, ψ, H)
 
 
-heatmap(reshape(abs2.(ψt[1].data),(Npoints, Npointsy)))
+frame = Observable(1)
+
+fig, ax, hm = heatmap(@lift(reshape(abs2.(ψt[$frame].data),(Npoints, Npointsy))))
+
+record(fig, "hm3.mp4", 1:length(ψt); framerate = 30) do t
+    frame[] = t
+end
