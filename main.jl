e8c6d3ef30229d7e502fde553057cab4c7b9ec56
Alistair Keiller <alistair@keiller.net>
2023-01-31 18:51:36 -0800

    get interaction working

--- main.jl
+++ main.jl
@@ -39,36 +39,46 @@ function potential(x,y)
         0
     end
 end
-# potential(x,y) = exp(-(x^2 + y^2)/30.0)
 V = potentialoperator(b_comp_x, potential)
 
 
 H = LazySum(Hkinx_FFT, Hkiny_FFT, V)
 
+f = Figure()
 
-x0 = -10
+x0 = -5
 y0 = 0
-p0_x = 2
+p0_x = 3
 p0_y = 0
-σ = 2.0
+σ = 2
+
+sg = SliderGrid(f[2,1],
+    (label = "x0", range = -10:0.1:0, startvalue = x0),
+    (label = "y0", range = -5:0.1:5, startvalue = y0),
+    (label = "p0_x", range = 0:0.1:5, startvalue = p0_x),
+    (label = "p0_y", range = -2:0.1:2, startvalue = p0_y),
+    (label = "σ", range = 0:0.1:5, startvalue = σ),
+)
+simulate = Button(f[3,1]; label = "simulate", tellwidth = false)
 
 ψx = gaussianstate(b_position, x0, p0_x, σ)
 ψy = gaussianstate(b_positiony, y0, p0_y, σ)
 ψ = ψx ⊗ ψy
 
-T = collect(0.0:0.1:20.0)
+T = collect(0.0:0.1:5.0)
 tout, ψt = timeevolution.schroedinger(T, ψ, H)
-
-f = Figure()
 
 frame = Observable(1)
 
 heatmap(f[1,1][1,1],@lift(reshape(abs2.(ψt[$frame].data),(Npoints, Npointsy))))
 
-simulate = Button(f[2,1][1,1]; label = "simulate", tellwidth = false)
-
 on(simulate.clicks) do clicks
-    frame[] = 1
+    ψx = gaussianstate(b_position, to_value(sg.sliders[1].value), to_value(sg.sliders[3].value), to_value(sg.sliders[5].value))
+    ψy = gaussianstate(b_positiony, to_value(sg.sliders[2].value), to_value(sg.sliders[4].value), to_value(sg.sliders[5].value))
+    ψ = ψx ⊗ ψy
+
+    tout, ψt = timeevolution.schroedinger(T, ψ, H)
+
     @async for i in 1:length(ψt)
         frame[] = i
         sleep(1/30)
